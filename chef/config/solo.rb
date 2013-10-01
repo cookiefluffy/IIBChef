308c84ce3bcfc821398a75a3596760de42b50ac9
emma <hongling@ca.ibm.com>
2013-10-01 16:34:45 -0400

    test commit

---
+++ chef/config/solo.rb
@@ -0,0 +1,5 @@
+file_cache_path File.expand_path(File.join(File.dirname(__FILE__), '..', "cache"))
+cookbook_path File.expand_path(File.join(File.dirname(__FILE__), '..', "cookbooks"))
+log_level        :info
+log_location     STDOUT
+
---
+++ chef/cookbooks/mbtkchef/metadata.rb
@@ -0,0 +1,7 @@
+name             'mbtkchef'
+maintainer       'IBM'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures test'
+#long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
